d67931598922704526b66b0112644de89b05dabc
EvanReeves <Reevezy93@gmail.com>
2014-06-29 19:42:02 +1000

    Rewrote lost files from before initial commit. :/

---
+++ SMUS/MetaData.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.IO;
+using TagLib;
+
+namespace SMUS
+{
+    //Mostly to remove path conflicts with IO.
+    class MetaData : Tag, IDisposable
+    {
+        private readonly TagLib.File File;
+
+        public MetaData(string _path)
+        {
+            File = TagLib.File.Create(_path);
+            this.File = File;
+        }
+
+        [Obsolete("Use Dispose.")]
+        public override void Clear(){} //Why not use dispose?
+
+        public override TagTypes TagTypes
+        { get { throw new System.NotImplementedException(); } }
+
+        public void Dispose()
+        {
+            this.File.Dispose();
+        }
+    }
+}
---
+++ SMUS/Song.cs
@@ -0,0 +1,31 @@
+﻿using SFML.Graphics;
+
+namespace SMUS
+{
+    //Contains both render and song data.
+    class Song : Text
+    {
+        public string Name { get; private set; }
+        public string Path { get; private set; }
+        public MetaData MetaData { get; private set; }
+
+        public Song(string _path, Font _font) : base("SongText", _font)
+        {
+            MetaData = new MetaData(_path);
+            Path = _path;
+            Name = MetaData.Title;
+            this.DisplayedString = Name;
+        }
+
+        public void Draw(RenderWindow window)
+        {
+            window.Draw(this);
+        }
+
+        public void Play()
+        {
+            //TODO: play song.
+        }
+
+    }
+}
